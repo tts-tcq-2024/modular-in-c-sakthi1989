3d18bf4b4a162aa4be65a64762ff223e98a07120
sakthi1989 <51967798+sakthi1989@users.noreply.github.com>
2024-07-21 22:02:41 +0530

    ColorPair.h

---
+++ ColorPair.h
@@ -0,0 +1,23 @@
+#ifndef COLOR_CODE_H
+#define COLOR_CODE_H
+
+enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+
+typedef struct {
+  enum MajorColor majorColor;
+  enum MinorColor minorColor;
+} ColorPair;
+
+const char* MajorColorNames[];
+const char* MinorColorNames[];
+
+int numberOfMajorColors;
+int numberOfMinorColors;
+
+void ColorPairToString(const ColorPair* colorPair, char* buffer);
+ColorPair GetColorFromPairNumber(int pairNumber);
+int GetPairNumberFromColor(const ColorPair* colorPair);
+void PrintColorCode(void);
+
+#endif
