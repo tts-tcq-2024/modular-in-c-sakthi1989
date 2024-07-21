72a1ff8f3ae44e3f9614dc5ecef9649c9b68aac7
sakthi1989 <51967798+sakthi1989@users.noreply.github.com>
2024-07-21 22:42:57 +0530

    ColorPair.h

--- ColorPair.h
+++ ColorPair.h
@@ -1,8 +1,6 @@
 #ifndef COLOR_CODE_H
 #define COLOR_CODE_H
 
-const int MAX_COLORPAIR_NAME_CHARS = 16;
-
 enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
 enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
 
