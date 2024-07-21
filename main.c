160704c1cec5e1649e5b304dace1f064e277a2bf
sakthi1989 <51967798+sakthi1989@users.noreply.github.com>
2024-07-21 22:37:59 +0530

    main.c

--- main.c
+++ main.c
@@ -2,14 +2,5 @@
 
 int main() {
     PrintColorCode();
-
-    #if 0
-    testNumberToPair(4, WHITE, BROWN);
-    testNumberToPair(5, WHITE, SLATE);
-
-    testPairToNumber(BLACK, ORANGE, 12);
-    testPairToNumber(VIOLET, SLATE, 25);
-    #endif
-
     return 0;
 }
