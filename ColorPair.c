9e2899b2e636803ace0c25e838483697dbf0f346
sakthi1989 <51967798+sakthi1989@users.noreply.github.com>
2024-07-21 22:38:50 +0530

    ColorPair.c

--- ColorPair.c
+++ ColorPair.c
@@ -10,7 +10,7 @@ int numberOfMajorColors =
 const char* MinorColorNames[] = {
   "Blue", "Orange", "Green", "Brown", "Slate"
 };
-const int MAX_COLORPAIR_NAME_CHARS = 16;
+
 int numberOfMinorColors =
   sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
 
